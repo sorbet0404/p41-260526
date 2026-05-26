2af632179b11430eded0ea77aeca538578b21cb6
sorbet0404 <cyclebae@naver.com>
2026-05-26 14:26:16 +0900

    s3클라이언트 적용-테스트 나눔 버전

---
+++ src/main/kotlin/com/back/HomeController.kt
@@ -0,0 +1,21 @@
+package com.back
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HomeController(
+    private val s3Service: S3Service
+) {
+
+    @GetMapping
+    fun main(): String {
+        return "Hi"
+    }
+
+    @GetMapping("/buckets")
+    fun buckets(): List<String> {
+        return s3Service.getBucketNames()
+    }
+
+}
